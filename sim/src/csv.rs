d6b56dea6201fcb9ed9aaf11e9cf0e5949757331
seb blanchet <s3blanch@uwaterloo.ca>
2024-11-01 13:13:21 -0400

    contd state callback

--- sim/src/csv.rs
+++ sim/src/csv.rs
@@ -2,11 +2,12 @@ use csv::Writer;
 use std::fs::OpenOptions;
 
 pub fn save(path: String, vec: Vec<String>) {
-    let file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(path)
-        .unwrap(); // TODO
+    let r = OpenOptions::new().create(true).append(true).open(path);
+    let file = if let Ok(f) = r {
+        f
+    } else {
+        return;
+    };
 
     let mut wtr = Writer::from_writer(file);
     wtr.write_record(vec).unwrap_or_default();
