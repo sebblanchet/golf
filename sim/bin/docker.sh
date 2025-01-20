9543582917bf9357735633344ca7af6a3308e6af
seb blanchet <s3blanch@uwaterloo.ca>
2025-01-20 08:20:50 -0500

    clean docker build + add loader to index

--- sim/bin/docker.sh
+++ sim/bin/docker.sh
@@ -2,10 +2,21 @@
 
 set -ex
 
-# build and start container
-docker build -t golf-sim .
-id=$(docker run -d --name golf-sim -it -v wasm:/out golf-sim sh)
+name="golf-sim"
 
-# extract files for container
-docker cp golf-sim:/out /tmp
-docker kill $id
+function stop() {
+  docker stop "$name" || true && docker rm "$name" || true
+}
+
+function build() {
+  stop
+
+  # build and start container
+  docker build -t "$name" .
+  docker run -d --name "$name" -p 9002:9002 "$name"
+
+  # extract files for container
+  docker cp "$name":/out/ out
+}
+
+build
